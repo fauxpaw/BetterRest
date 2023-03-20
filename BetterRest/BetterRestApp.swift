a0f2c797fe211e896f9e3f1d840fe0ccc29442b3
Fauxpaw <fauxpawsc@live.com>
2023-03-20 14:24:01 -0700

    final

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Michael Sweeney on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
